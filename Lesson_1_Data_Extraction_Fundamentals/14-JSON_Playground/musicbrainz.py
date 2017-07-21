4eb3e86b64fa69ea929b33f11a6825c06c4da90c
Vitor Sexto Bernardes <vitorsb@gmail.com>
2017-07-21 10:39:05 -0300

    Find disambiguation for Nirvana

--- Lesson_1_Data_Extraction_Fundamentals/14-JSON_Playground/musicbrainz.py
+++ Lesson_1_Data_Extraction_Fundamentals/14-JSON_Playground/musicbrainz.py
@@ -41,19 +41,17 @@ def pretty_print(data, indent=4):
 
 
 def main():
-    results = query_by_name(ARTIST_URL, query_type["simple"], "Queen")
+    results = query_by_name(ARTIST_URL, query_type["simple"], "Nirvana")
     pretty_print(results)
 
     for artist_data in results['artists']:
         art_name = artist_data.get('name')
         disamb = artist_data.get('disambiguation')
-        if "begin-area" in artist_data:
-            begin_area_name = artist_data['begin-area'].get('name', 'Not available')
-        else:
-            begin_area_name = 'Not available'
-        print(u"Nome: {:s}, Disambiguation: {:s}, Area name: {:s}".format(art_name,\
-                                                                          disamb,\
-                                                                          begin_area_name))
+        # if "begin-area" in artist_data:
+        #     begin_area_name = artist_data['begin-area'].get('name', 'Not available')
+        # else:
+        #     begin_area_name = 'Not available'
+        print(u"Nome: {:s}, Disambiguation: {:s}".format(art_name, disamb))
 
     # artist_id = results["artists"][1]["id"]
     # print "\nARTIST:"
